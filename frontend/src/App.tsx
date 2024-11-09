96d6968ccc56769d0c7d0c716c343452b5601896
daniel <chididaniel1998@gmail.com>
2024-11-08 17:17:14 -0800

    done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,6 @@ import Sidebar from "./components/Sidebar";
 import Banner from "../src/assets/banner.png";
 import Big from "../src/assets/big.png";
 import Buttons from "../src/assets/buttons.png";
-import Dots from "../src/assets/Dots.svg";
 import { ToastContainer } from "react-toastify";
 
 const App: React.FC = () => {
@@ -20,7 +19,7 @@ const App: React.FC = () => {
   const [isHotelModalOpen, setIsHotelModalOpen] = useState<boolean>(false);
   const [isAttractionlModalOpen, setIsAttractionModalOpen] =
     useState<boolean>(false);
-    const [refreshTrigger, setRefreshTrigger] = useState(false);
+  const [refreshTrigger, setRefreshTrigger] = useState(false);
   const openFlightModal = () => setIsFlightModalOpen(true);
 
   const openAttractionModal = () => setIsAttractionModalOpen(true);
@@ -43,108 +42,106 @@ const App: React.FC = () => {
     toggleRefresh();
   };
 
-  
-
   return (
     <>
       <ToastContainer />
       <Navbar />
       <div className="flex">
-      
-
         <Sidebar />
 
         <div className="App bg-[#F0F2F5]  flex-1  lg:p-4 xl:p-6 xl:ml-64 px-2 mt-14 w-screen md:mx-3 my-5">
           <div className="bg-white md:p-10 lg:p-3">
+            <img src={Banner} className="w-full  mb-5" alt="" />
+            <div className="flex justify-between my-3">
+              <div className="flex items-center bg-[#F5E4CE] px-2 py-1 me-3 rounded-md">
+                <span role="img" aria-label="calendar" className="mr-1">
+                  📅
+                </span>
+                <span>21 March 2024</span>
+                <span className="mx-1">→</span>
+                <span>21 April 2024</span>
+              </div>
 
-        
-          <img src={Banner} className="w-full  mb-5" alt="" />
-          <div className="flex justify-between my-3">
-            <div className="flex items-center bg-[#F5E4CE] px-2 py-1 me-3 rounded-md">
-              <span role="img" aria-label="calendar" className="mr-1">
-                📅
-              </span>
-              <span>21 March 2024</span>
-              <span className="mx-1">→</span>
-              <span>21 April 2024</span>
+              <div className="flex">
+                <img className=" cursor-pointer md:me-3" src={Buttons} alt="" />
+              </div>
+            </div>
+            <div className="flex justify-between my-3">
+              <div className="text-[#000000] font-semibold text-xl md:text-3xl">
+                Bahamas Family Trip <br />{" "}
+                <span className="md:text-lg font-medium text-[#676E7E]">
+                  New York, United States of America | Solo Trip
+                </span>
+              </div>
+              <div className=" md:me-5">
+                {" "}
+                <img src={Big} alt="" />
+              </div>
             </div>
 
-            <div className="flex"><img className=" cursor-pointer md:me-3" src={Buttons} alt="" /><img className=" cursor-pointer" src={Dots} alt="" /></div>
-          </div>
-          <div className="flex justify-between my-3">
-            <div className="text-[#000000] font-semibold text-xl md:text-3xl">
-              Bahamas Family Trip <br />{" "}
-              <span className="md:text-lg font-medium text-[#676E7E]">
-                New York, United States of America | Solo Trip
-              </span>
+            <div className="flex flex-col md:flex-row md:space-x-2 gap-y-4 my-7">
+              <Card
+                title="Activities"
+                description="Build, personalize, and optimize your itineraries with our trip planner."
+                buttonLabel="Add Activities"
+                onButtonClick={openAttractionModal}
+                bgColor="bg-[#000031]"
+                textColor="text-white"
+                btcolor="bg-[#0D6EFD]"
+                btext="text-white"
+              />
+              <Card
+                title="Hotels"
+                description="Build, personalize, and optimize your itineraries with our trip planner."
+                buttonLabel="Add Hotels"
+                onButtonClick={openHotelModal}
+                bgColor="bg-[#E7F0FF]"
+                textColor="text-black"
+                btcolor="bg-[#0D6EFD]"
+                btext="text-white"
+              />
+              <Card
+                title="Flights"
+                description="Build, personalize, and optimize your itineraries with our trip planner."
+                buttonLabel="Add Flights"
+                onButtonClick={openFlightModal}
+                bgColor="bg-[#0D6EFD]"
+                textColor="text-white"
+                btcolor="bg-white"
+                btext="text-[#0D6EFD]"
+              />
             </div>
-            <div className=" md:me-5"> <img src={Big} alt="" /></div>
-          </div>
 
-          <div className="flex flex-col md:flex-row md:space-x-2 gap-y-4 my-7">
-            <Card
-              title="Activities"
-              description="Build, personalize, and optimize your itineraries with our trip planner."
-              buttonLabel="Add Activities"
-              onButtonClick={openAttractionModal}
-              bgColor="bg-[#000031]"
-              textColor="text-white"
-              btcolor="bg-[#0D6EFD]"
-              btext="text-white"
-            />
-            <Card
-              title="Hotels"
-              description="Build, personalize, and optimize your itineraries with our trip planner."
-              buttonLabel="Add Hotels"
-              onButtonClick={openHotelModal}
-              bgColor="bg-[#E7F0FF]"
-              textColor="text-black"
-              btcolor="bg-[#0D6EFD]"
-              btext="text-white"
-            />
-            <Card
-              title="Flights"
-              description="Build, personalize, and optimize your itineraries with our trip planner."
-              buttonLabel="Add Flights"
-              onButtonClick={openFlightModal}
-              bgColor="bg-[#0D6EFD]"
-              textColor="text-white"
-              btcolor="bg-white"
-              btext="text-[#0D6EFD]"
-            />
-          </div>
+            {isFlightModalOpen && <FlightSearch onClose={closeFlightModal} />}
+            {isHotelModalOpen && <HotelSearch onClose={closeHotelModal} />}
+            {isAttractionlModalOpen && (
+              <AttractionSearch onClose={closeAttractionModal} />
+            )}
 
-          {isFlightModalOpen && <FlightSearch onClose={closeFlightModal} />}
-          {isHotelModalOpen && <HotelSearch onClose={closeHotelModal} />}
-          {isAttractionlModalOpen && (
-            <AttractionSearch onClose={closeAttractionModal} />
-          )}
-
-          <div className="">
-            {" "}
-            <h3 className="text-[#1D2433] text-2xl font-semibold">
-              Trip itineraries
-            </h3>{" "}
-            <p className="#647995 font-medium">
-              Your trip itineraries are placed here
-            </p>{" "}
-          </div>
+            <div className="">
+              {" "}
+              <h3 className="text-[#1D2433] text-2xl font-semibold">
+                Trip itineraries
+              </h3>{" "}
+              <p className="#647995 font-medium">
+                Your trip itineraries are placed here
+              </p>{" "}
+            </div>
           </div>
           <div className="bg-white pb-4">
-          <FlightCard
-            refreshTrigger={refreshTrigger}
-            onButtonClick={openFlightModal}
-          />
-          <HotelCard
-            refreshTrigger={refreshTrigger}
-            onButtonClick={openHotelModal}
-          />
-          <AttractionCard
-            refreshTrigger={refreshTrigger}
-            onButtonClick={openAttractionModal}
-          />
+            <FlightCard
+              refreshTrigger={refreshTrigger}
+              onButtonClick={openFlightModal}
+            />
+            <HotelCard
+              refreshTrigger={refreshTrigger}
+              onButtonClick={openHotelModal}
+            />
+            <AttractionCard
+              refreshTrigger={refreshTrigger}
+              onButtonClick={openAttractionModal}
+            />
           </div>
-         
         </div>
       </div>
     </>
--- frontend/src/components/FlightCard.tsx
+++ frontend/src/components/FlightCard.tsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from "react";
-import { FaMapMarkerAlt, FaStar } from "react-icons/fa";
+// import { FaMapMarkerAlt, FaPlane, FaStar } from "react-icons/fa";
 import ActiviteImage from "../assets/Airplain.svg";
 import X from "../assets/X.png";
-import Pool from "../assets/Pool.svg";
-import Bar from "../assets/Wine.svg";
-import Bed from "../assets/Bed.svg";
-import Calendar from "../assets/Calendar.svg";
-
-
+// import Pool from "../assets/Pool.svg";
+// import Bar from "../assets/Wine.svg";
+// import Bed from "../assets/Bed.svg";
+// import Calendar from "../assets/Calendar.svg";
+import Duration from "../assets/duration.png";
+import {
+  FaPlaneDeparture,
+  FaPlaneArrival,
+  FaLuggageCart,
+  FaUtensils,
+  FaTv,
+  FaUsb,
+} from "react-icons/fa";
 
 interface Flight {
   iataCode: string;
@@ -20,138 +27,166 @@ interface Flight {
   representativePrice?: { chargeAmount?: number };
 }
 interface ChildComponentProps {
-    onButtonClick: () => void;
-  }
+  onButtonClick: () => void;
+}
 
-const FlightCard: React.FC<ChildComponentProps & { refreshTrigger: boolean }> = ({ onButtonClick,refreshTrigger  }) => {
-    const [savedFlights, setSavedFlights] = useState<Flight[]>([]);
+const FlightCard: React.FC<
+  ChildComponentProps & { refreshTrigger: boolean }
+> = ({ onButtonClick, refreshTrigger }) => {
+  const [savedFlights, setSavedFlights] = useState<Flight[]>([]);
 
-    useEffect(() => {
-      // Retrieve saved hotels from localStorage on component mount
-      const flights = localStorage.getItem("savedFlights");
-  
-      if (flights) {
-        const parsedFlights = JSON.parse(flights);
-        setSavedFlights(parsedFlights);
-      }
-     
-    }, [refreshTrigger]);
+  useEffect(() => {
+    // Retrieve saved hotels from localStorage on component mount
+    const flights = localStorage.getItem("savedFlights");
+
+    if (flights) {
+      const parsedFlights = JSON.parse(flights);
+      setSavedFlights(parsedFlights);
+    }
+  }, [refreshTrigger]);
   return (
     <div className="p-6 bg-[#F0F2F5] mb-10 mx-3   rounded-md">
-    <div className="flex justify-between mb-5">
-      <div className="flex items-center">
-        <img
-          src={ActiviteImage}
-          alt=""
-          className="mr-4 w-6 h-6 object-contain"
-        />
-        <span className="text-xl font-semibold text-[#1D2433]">Flights</span>
+      <div className="flex justify-between mb-5">
+        <div className="flex items-center">
+          <img
+            src={ActiviteImage}
+            alt=""
+            className="mr-4 w-6 h-6 object-contain"
+          />
+          <span className="text-xl font-semibold text-[#1D2433]">Flights</span>
+        </div>
+        {savedFlights.length > 0 ? (
+          <button
+            onClick={onButtonClick}
+            className="text-[#0D6EFD] bg-white px-4 py-2 rounded font-semibold hover:bg-[#d1d1d1]"
+          >
+            Add Flights
+          </button>
+        ) : (
+          ""
+        )}
       </div>
-      {savedFlights.length > 0?     <button onClick={onButtonClick} className="text-[#0D6EFD] bg-white px-4 py-2 rounded font-semibold hover:bg-[#d1d1d1]">
-        Add Flights
-      </button>:''}
-   
-    </div>
 
-    {savedFlights.length > 0 ? (
-      savedFlights.map((airline, index) => (
-        <div className="flex gap-0 mb-4" key={index}>
-          <div className="w-full px-2 border rounded-l-md shadow-sm bg-gray-50">
-            <div className="flex md:flex-col flex-col">
-              <img
-                src={airline.logoUrl}
-                alt={ "flight"}
-                className="w-fit rounded-md object-cover mb-4 md:mb-0"
-              />
-              <div className="flex-1 sm:ml-4">
-                <div className="flex justify-between items-center mb-2">
-                  <h4 className="text-2xl font-semibold text-black">
-                    {airline.name|| "Unnamed flight"}
-                  </h4>
-                  <div className="text-right text-[#1D2433] font-semibold">
-                    {/* <p className="text-3xl">
-                      $
-                      {Math.floor(
-                        airline.property.priceBreakdown.grossPrice.value || 0
-                      )}
-                    </p> */}
-                    <p className="text-md text-[#1D2433]">
-                      10:30 AM on Mar 19
-                    </p>
-                  </div>
-                </div>
-
-                <p className="text-[#1D2433] text-lg font-medium mb-2 md:max-w-[50%]">
-                  {airline.iataCode || "No description available."}
-                </p>
-
-                <div className="flex items-center space-x-2 text-gray-600 text-sm">
-                  <FaMapMarkerAlt className="text-blue-500" />
-                  <span className="font-semibold text-[#0D6EFD] text-lg">
-                    Show in map
-                  </span>
-                  <FaStar className="text-yellow-400 ml-4" />
-                  <span className="text-lg">
-                    {airline.reviewsStats?.combinedNumericStats.average ||
-                      "N/A"}{" "}
-                    ({airline.reviewsStats?.combinedNumericStats.total || "0"})
-                  </span>
-                  <span className="flex items-start space-x-2">
-                    <img src={Bed} alt="Bed" className="w-6 h-6" />
-                    <span>Bed</span>
-                  </span>
-                </div>
-
-                <div className="border-y border-[#E4E7EC] w-full text-lg text-[#647995] py-2 mt-2">
-                  <div className="flex justify-between pb-3">
-                    <div className="flex gap-x-3">
-                      <span>Facilities:</span>
-                      <span className="flex items-start space-x-2">
-                        <img src={Pool} alt="calendar" className="w-6 h-6" />
-                        <span>Pool</span>
-                      </span>
-                      <span className="flex items-start space-x-2">
-                        <img src={Bar} alt="calendar" className="w-6 h-6" />
-                        <span>Bar</span>
-                      </span>
+      {savedFlights.length > 0 ? (
+        savedFlights.map((airline, index) => (
+          <div className="flex gap-0 mb-4" key={index}>
+            <div className="w-full  border rounded-l-md md:p-10 p-10 shadow-sm bg-gray-50">
+              <div className="md:flex md:flex-col flex-col">
+                <div className="md:flex justify-between">
+                  <div className="flex gap-x-2">
+                    <div>
+                      {" "}
+                      <img
+                        src={airline.logoUrl}
+                        alt={"flight"}
+                        className="md:w-14 mx-auto"
+                      />
                     </div>
-                    <span className="flex items-start space-x-2">
-                      <img src={Calendar} alt="pool" className="w-6 h-6" />
-                      <span>Check In: 20-04-2024</span>
-                      <img src={Calendar} alt="pool" className="w-6 h-6" />
-                      <span>Check In: 20-04-2024</span>
-                    </span>
+                    <div>
+                      <span className="font-semibold text-lg text-[#1D2433]">
+                        {airline.name || "Unnamed flight"}
+                      </span>
+                      <br />
+                      <span className="text-[#676E7E] font-medium">
+                        AA-829
+                      </span>{" "}
+                      <button className="p-1 bg-[#0A369D] rounded-md px-3 text-white">
+                        first class
+                      </button>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="md:flex flex-col md:flex-row items-center text-center md:space-x-4 mt-4 md:mt-0">
+                      <div>
+                        <p className="text-2xl font-semibold">08:35</p>
+                        <p className="text-sm text-gray-500">Sun, 20 Aug</p>
+                        <p className="font-semibold">LOS</p>
+                      </div>
+
+                      <div className="flex flex-col items-center justify-center space-y-2 mt-2 md:mt-0">
+                        <div className="flex items-center space-x-20">
+                          <FaPlaneDeparture className="text-gray-400  " />
+                          <span className="text-blue-500">
+                            Duration: 1h 45m
+                          </span>
+                          <FaPlaneArrival className="text-gray-400" />
+                        </div>
+                        {/* Icon below duration */}
+                        <img src={Duration} alt="" />
+                      </div>
+
+                      <div>
+                        <p className="text-2xl font-semibold">09:55</p>
+                        <p className="text-sm text-gray-500">Sun, 20 Aug</p>
+                        <p className="font-semibold">SIN</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    {/* Price */}
+                    <div className="text-right mt-4 md:mt-0">
+                      <p className="text-2xl font-bold text-gray-800">
+                        {" "}
+                        $123.00
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                {/* Facilities */}
+                <div className="border-y mt-4 py-4">
+                  <div className="flex flex-wrap items-center space-x-4 text-gray-600">
+                    <p className="font-semibold">Facilities:</p>
+                    <div className="flex items-center space-x-2">
+                      <FaLuggageCart />
+                      <span>Baggage: 20kg, Cabin Baggage: 8kg</span>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <FaTv />
+                      <span>In-flight entertainment</span>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <FaUtensils />
+                      <span>In-flight meal</span>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <FaUsb />
+                      <span>USB Port</span>
+                    </div>
                   </div>
                 </div>
 
-                <div className="flex justify-between font-medium text-[#0D6EFD] mt-3">
-                  <div className="flex gap-x-4">
-                    <span>Hotel details</span>
-                    <span>Price details</span>
-                  </div>
-                  <span>Edit details</span>
+                <div className="flex gap-x-5 text-blue-500 mt-4">
+                  <a href="#" className="hover:underline">
+                    Flight details
+                  </a>
+                  <a href="#" className="hover:underline">
+                    Price details
+                  </a>
+                  <a href="#" className="hover:underline">
+                    Edit details
+                  </a>
                 </div>
               </div>
             </div>
+            <div className="w-8 md:w-16 bg-[#FBEAE9] flex justify-center items-center rounded-r-md">
+              <img src={X} className="w-6 h-6 cursor-pointer" alt="" />
+            </div>
           </div>
-
-          <div className="w-8 md:w-16 bg-[#FBEAE9] flex justify-center items-center rounded-r-md">
-            <img src={X} className="w-6 h-6 cursor-pointer" alt="" />
-          </div>
-        </div>
-      ))
-    ) : (
-
+        ))
+      ) : (
         <div className="bg-white p-8 md:p-14 xl:p-24 flex flex-col justify-center items-center space-y-4">
-        <h3>No Request yet</h3> 
-        <button onClick={onButtonClick} className="text-white bg-[#0D6EFD] px-4 py-2 rounded font-semibold hover:bg-[#0d6dfdab]">
-          Add Flights
-        </button>
-      </div>
-      
-    )}
-  </div>
-  )
-}
+          <h3>No Request yet</h3>
+          <button
+            onClick={onButtonClick}
+            className="text-white bg-[#0D6EFD] px-4 py-2 rounded font-semibold hover:bg-[#0d6dfdab]"
+          >
+            Add Flights
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default FlightCard
+export default FlightCard;
