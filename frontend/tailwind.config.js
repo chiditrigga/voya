7af8bbfc2cc80346013987506c2ecb2a6c039899
daniel <chididaniel1998@gmail.com>
2024-11-12 05:26:27 -0800

    reverse

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from 'tailwindcss/defaultTheme';
-
 export default {
   content: [
     "./index.html",
@@ -9,19 +7,13 @@ export default {
   theme: {
     extend: {
       gridTemplateColumns: {
+       
         '16': 'repeat(16, minmax(0, 1fr))',
         '24': 'repeat(24, minmax(0, 1fr))',
-      },
-      fontSize: {
-        ...Object.fromEntries(
-          Object.entries(defaultTheme.fontSize).map(([key, value]) => {
-            const [fontSize, options] = Array.isArray(value) ? value : [value];
-            const scaledSize = parseFloat(fontSize) * 0.9 + 'rem';
-            return [key, [scaledSize, options]];
-          })
-        ),
-      },
+      
+      }
     },
   },
   plugins: [],
 }
+
